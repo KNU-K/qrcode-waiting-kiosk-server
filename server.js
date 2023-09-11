3d0c81e3a5e32eb3d1d80149c222a2b9fcd1f615
KNU-K <knukang334@gmail.com>
2023-09-11 18:32:28 +0900

    #4 😎feat/qr-code : redis 연결 완료
    db보다는 redis가 io에 최적화 되있는거 같아서 redis로 변경함.

--- server.js
+++ server.js
@@ -2,7 +2,21 @@ const express = require("express");
 const app = express();
 const routers = require("./routers/index");
 const { conn } = require("./config/mysql");
+const { client } = require("./config/redis");
+const session = require("express-session");
+const passport = require("passport");
 const port = process.env.PORT | 8000;
+
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: false,
+    resave: false,
+    cookie: { httpOnly: true },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -11,7 +25,9 @@ app.use("/api", routers);
 app.listen(port, async () => {
   try {
     await conn.connect();
-    console.log("db conn succeed");
+    console.log("db conn test succeed");
+    await client.connect();
+    console.log("redis conn test succeed");
     console.log(`server open ${port}`);
   } catch (err) {
     console.err(err.message);
